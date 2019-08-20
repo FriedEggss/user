889b7ca66504e12c5c1604eaaa7f2cdde2a79546
FriedEggss <495667622@qq.com>
2019-08-20 20:39:07 +0800

    ok

--- src/main/java/com/woniu/woniuticket/user/test/GitDemo.java
+++ src/main/java/com/woniu/woniuticket/user/test/GitDemo.java
@@ -2,6 +2,7 @@ package com.woniu.woniuticket.user.test;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("aaaa");
+
+        System.out.println("aaaa");System.out.println("aaaa");System.out.println("aaaa");
     }
 }
